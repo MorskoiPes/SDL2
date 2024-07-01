6508ac95924cf42a37fc900dfd5b87e354316fdd
MorskoiPes <prokudsergo04@gmail.com>
2024-07-01 21:51:02 +0700

    создание фигур

--- Source.cpp
+++ Source.cpp
@@ -12,6 +12,17 @@ const int SCREEN_HEIGHT = 480;
 SDL_Window* win = NULL;
 SDL_Surface* scr = NULL;
 SDL_Surface* john = NULL;
+SDL_Renderer* ren = NULL;
+
+int quit() {
+    SDL_FreeSurface(john);
+
+    SDL_DestroyWindow(win);
+
+    SDL_Quit();
+
+    return 0;
+}
 
 int init() {
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
@@ -20,11 +31,19 @@ int init() {
         return 1;
     }
 
-    win = SDL_CreateWindow("Source", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    win = SDL_CreateWindow("Source", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_RESIZABLE);
     if (win == NULL) {
         std::cout << "Can't create window: " << SDL_GetError() << std::endl;
         system("pause");
         return 1;
+    }
+
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
+
+    if (ren == NULL) {
+        std::cout << "Can't create render:" << SDL_GetError() << std::endl;
+        system("pause");
+        return quit();
     }
 
     scr = SDL_GetWindowSurface(win);
@@ -44,15 +63,7 @@ int load() {
     return 0;
 }
 
-int quit() {
-    SDL_FreeSurface(john);
 
-    SDL_DestroyWindow(win);
-
-    SDL_Quit();
-
-    return 0;
-}
 int main(int argc, char** args) {
     if (init() == 1) {
         quit();
@@ -68,23 +79,23 @@ int main(int argc, char** args) {
     bool run = true;
     SDL_Event e;
 
-    SDL_Rect bg_flower;
-    bg_flower.w = SCREEN_WIDTH;
-    bg_flower.h = SCREEN_HEIGHT;
-    bg_flower.x = 0;
-    bg_flower.y = 0;
+    int x = 0;
+    int y = 0;
 
+    SDL_Rect bg;
+    bg.w = SCREEN_WIDTH;
+    bg.h = SCREEN_HEIGHT;
+    bg.x = x;
+    bg.y = y;
 
-    SDL_BlitScaled(john, NULL, scr, &bg_flower);
 
     while (run) {
         while (SDL_PollEvent(&e) != NULL) {
             if (e.type == SDL_KEYDOWN) {
-
                 if (e.key.keysym.sym == SDLK_ESCAPE) {
                     run = false;
                 }
-                
+   
             }
 
             if (e.type == SDL_QUIT) {
@@ -106,15 +117,19 @@ int main(int argc, char** args) {
                 }
             }
         }
-        r.x = x;
-        r.y = y;
+        bg.x = x;
+        bg.y = y;
 
         SDL_FillRect(scr, NULL, SDL_MapRGB(scr->format, 255, 255, 255));
 
-        SDL_BlitSurface(john, NULL, scr, &r);
+        SDL_BlitScaled(john, NULL, scr, &bg);
 
         SDL_UpdateWindowSurface(win);
     }
+
+    SDL_Rect fillRect = { SCREEN_HEIGHT / 2, SCREEN_WIDTH / 2, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+    SDL_SetRenderDrawColor(ren, 255,0,0,255);
+    SDL_RenderFillRect(ren,&fillRect);
 
     quit();
 }
